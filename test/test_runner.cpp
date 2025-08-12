e29c5db9c31f28e42af1ad3b74376fcd9535ec6e
William-Thomas-Andrews <wtandrews05@gmail.com>
2025-08-12 14:52:01 -0400

    Change file structure

--- test/test_runner.cpp
+++ test/test_runner.cpp
@@ -8,20 +8,12 @@
 
 
 void run_all_tests() {
-    // // Input data test
-    // std::string str = "../src/data/data_2.txt";
-    // Graph Gi = Graph(str);
-    // std::cout << Gi << std::endl;
-    // Vertex v0(0, -1, -7); Vertex v3(3, 4, 1);
-    // auto [dist, prev] = Gi.Dijkstra(v0, v3);
-    // // print_path(prev, 0, 3); // TODO: Fix linking error
-
     Graph G;
     Vertex begin, end;
     std::string file_path;
     std::tuple<Vertex, Vertex> vertex_pair; 
     for (int i = 0; i < 10; i++) {
-        file_path = std::format("../src/data/data_{}.txt", i);
+        file_path = std::format("../src/data/set/data_{}.txt", i);
         G = Graph(file_path);
         std::cout << G << std::endl;
         vertex_pair = G.get_random_vertex_pair();
@@ -31,6 +23,5 @@ void run_all_tests() {
         auto [dist, prev] = G.Dijkstra(begin, end);
         print_path(prev, begin.val, end.val); // TOD: Fix linking error
     }
-
     std::cout << "All tests done!." << std::endl;
 }
